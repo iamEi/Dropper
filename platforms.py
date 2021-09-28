d4c72a474f9bd3ad041fdf0914fa2309964ee03e
iamEi <79460406+iamEi@users.noreply.github.com>
2021-09-28 10:44:54 +0800

    speed up feature added

--- platforms.py
+++ platforms.py
@@ -2,17 +2,16 @@ import pygame
 from random import randint
 
 class Platforms(pygame.sprite.Sprite):
-	def __init__(self):
+	def __init__(self,speed):
 		super().__init__()
-
-
+		self.speed = speed
 		img = pygame.image.load('images/platform_2.png')
 		self.image = pygame.transform.scale(img, (randint(150,200), 35))
 		self.rect = self.image.get_rect(center = (randint(0,500),randint(705,720)))
 
 
 	def move(self):
-		self.rect.y -= 4
+		self.rect.y -= self.speed
 
 	def destroy(self):
 		if self.rect.y < -50:
